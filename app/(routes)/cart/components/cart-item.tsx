a68aa9b769b0a3efab7fd4ffb3fb789ce9b2967f
Henry <168932407+ELperrocode@users.noreply.github.com>
2024-10-22 08:42:09 -0500

    Refactor default theme to "light" in RootLayout component

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -18,53 +18,53 @@ const CartItem = (props: CartItemProps) => {
   const { removeItem, incrementQuantity, decrementQuantity } = useCart();
 
   return (
-    <li className="flex flex-col sm:flex-row py-6 border-b ">
-      <div
-        
-        className="flex-shrink-0"
-      >
-        {product.imagen_producto && product.imagen_producto.length > 0 ? (
-          <img
-            src={product.imagen_producto[0].url}
-            alt={product.nombre}
-            className="w-24 h-24 overflow-hidden rounded-md sm:w-auto sm:h-36"
-          />
-        ) : (
-          <div className="w-24 h-24 overflow-hidden rounded-md sm:w-auto sm:h-32 bg-gray-200 flex items-center justify-center">
-            <span>No Image</span>
-          </div>
-        )}
-      </div>
-      <div className="flex justify-between flex-1 px-6">
-        <div>
-          <h2 className="text-lg font-bold">{product.nombre}</h2>
-          <p className="px-2 py-1 text-white bg-blue-950 rounded-full w-fit">{FormatPrice(product.precio)}</p>
-         
-          <div className="flex items-center gap-4 mt-4">
-            <button
-              className="px-4 py-2 text-white bg-gray-800 rounded-lg"
-              onClick={() => decrementQuantity(product.id)}
-            >
-              -
-            </button>
-            <span className="text-lg">{quantity}</span>
-            <button
-              className="px-4 py-2 text-white bg-gray-800 rounded-lg"
-              onClick={() => incrementQuantity(product.id)}
-            >
-              +
-            </button>
-          </div>
+    <li className="flex flex-col sm:flex-row py-6 border-b">
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full sm:flex">
+        <div className="sm:flex-shrink-0">
+          {product.imagen_producto && product.imagen_producto.length > 0 ? (
+            <img
+              src={product.imagen_producto[0].url}
+              alt={product.nombre}
+              className="w-full sm:w-48 h-48 object-cover"
+            />
+          ) : (
+            <div className="w-full sm:w-48 h-48 bg-gray-200 flex items-center justify-center">
+              <span>No Image</span>
+            </div>
+          )}
         </div>
-        <div className="flex items-center">
-          <button
-            className={cn(
-              "rounded-full flex items-center bg-white border shadow-md p-2 hover:scale-110 transition"
-            )}
-            onClick={() => removeItem(product.id)}
-          >
-            <X size={20} />
-          </button>
+        <div className="p-4 sm:px-6 sm:py-4 flex flex-col justify-between w-full">
+          <div>
+            <h2 className="text-lg font-bold mb-2">{product.nombre}</h2>
+            <p className="px-2 py-1 text-white bg-blue-950 rounded-full w-fit mb-4">
+              {FormatPrice(product.precio)}
+            </p>
+            <div className="flex items-center gap-4">
+              <button
+                className="px-4 py-2 text-white bg-gray-800 rounded-lg"
+                onClick={() => decrementQuantity(product.id)}
+              >
+                -
+              </button>
+              <span className="text-lg">{quantity}</span>
+              <button
+                className="px-4 py-2 text-white bg-gray-800 rounded-lg"
+                onClick={() => incrementQuantity(product.id)}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-end mt-4 sm:mt-0">
+            <button
+              className={cn(
+                "rounded-full flex items-center bg-white border shadow-md p-2 hover:scale-110 transition"
+              )}
+              onClick={() => removeItem(product.id)}
+            >
+              <X size={20} />
+            </button>
+          </div>
         </div>
       </div>
     </li>
